ab066d8450a76503bf6ea57a85df5a3fdbf00c0d
masprog2022 <masprog2022@gmail.com>
2022-01-13 11:07:39 +0100

    Find movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/MovieDTO.java
@@ -0,0 +1,78 @@
+package com.devsuperior.dsmovie.dto;
+
+import com.devsuperior.dsmovie.entities.Movie;
+
+public class MovieDTO {
+	
+	private Long id;
+	private String title;
+	private Integer count;
+	private Integer score;
+	private String image;
+	
+	public MovieDTO() {
+		
+	}
+
+	public MovieDTO(Long id, String title, Integer count, Integer score, String image) {
+
+		this.id = id;
+		this.title = title;
+		this.count = count;
+		this.score = score;
+		this.image = image;
+	}
+	
+	public MovieDTO(Movie movie) {
+
+		id = movie.getId();
+		title = movie.getTitle();
+		count = movie.getCount();
+		score = movie.getScore();
+		image = movie.getImage();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+	public Integer getScore() {
+		return score;
+	}
+
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	 
+	
+
+}
